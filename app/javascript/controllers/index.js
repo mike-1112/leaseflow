a75743547f02396430a3c3b6ee23d2d65c19a7c0
mike-1112 <yahmen10565@gmail.com>
2025-06-12 22:38:56 +0930

    approval working? need to confirm4

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,10 @@
     import RentalApplicationController from "./rental_application_controller"
     import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
     import ToastController from "./toast_controller"
+    import RentalFormController from "./rental_form_controller"
     application.register("toast", ToastController)
     application.register("rental-application", RentalApplicationController)
+
+        application.register("rental-form", RentalFormController)
 
     eagerLoadControllersFrom("controllers", application)
