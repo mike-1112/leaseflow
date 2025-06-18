da1eca8ab569f6a2c994364e2a0631b60cbafd58
mike-1112 <yahmen10565@gmail.com>
2025-06-19 02:08:05 +0930

    approve reject stil isnt working4

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,3 +1,5 @@
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import RentalApplicationController from "./rental_application_controller"
 eagerLoadControllersFrom("controllers", application)
+application.register("rental-application", RentalApplicationController)
