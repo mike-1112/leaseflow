b9d2544e86a2cd2bbb6cdd10cc9bcd538037f4a1
mike-1112 <yahmen10565@gmail.com>
2025-06-25 03:58:13 +0930

    yep3

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,19 +3,11 @@
 import { application } from "./application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-// Manually-import any controllers that need explicit registration:
 import RentalApplicationController from "./rental_application_controller"
-import FilterController             from "./filter_controller"
-import RentalFormController         from "./rental_form_controller"
-import StateComplianceController    from "./state_compliance_controller"
-import ToastController              from "./toast_controller"
+import FilterController             from "./filter_controller"    // ← import it
 
-// Auto-load all other controllers in this directory:
 eagerLoadControllersFrom("controllers", application)
 
-// Explicitly register key controllers:
-application.register("rental-application",    RentalApplicationController)
-application.register("filter",                FilterController)
-application.register("rental-form",           RentalFormController)
-application.register("state-compliance",      StateComplianceController)
-application.register("toast",                 ToastController)
+// explicitly register both:
+application.register("rental-application", RentalApplicationController)
+application.register("filter",             FilterController)
